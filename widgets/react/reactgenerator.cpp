77a1640955f05da4f233a9882d5091a18caab47e
Kapil <kapil.pipaliya@yahoo.com>
2021-04-25 23:43:57 +0530

    make react widget

--- widgets/react/reactgenerator.cpp
+++ widgets/react/reactgenerator.cpp
@@ -1,5 +1,6 @@
 #include "./reactgenerator.h"
 #include "pystring/pystring.hpp"
+#include <QSizePolicy>
 namespace widgets
 {
 ReactGenerator::ReactGenerator(QWidget *parent) : QWidget(parent)
@@ -11,38 +12,39 @@ ReactGenerator::~ReactGenerator() {}
 void ReactGenerator::init()
 {
   auto v1 = new QVBoxLayout(this);
-  auto v1_h1 = new QHBoxLayout();
-  exportgenerator(v1_h1);
-  auto v1_h2 = new QHBoxLayout();
-  reactivestatgenerator(v1_h2);
-  auto v1_h3 = new QHBoxLayout();
-  elementdirectivesgenerator(v1_h3);
   auto v1_h4 = new QHBoxLayout();
   importgenerator(v1_h4);
+  auto v1_h2 = new QHBoxLayout();
+  componentSkeletonGenerator(v1_h2);
+  auto v1_h1 = new QHBoxLayout();
+  hookGenerator(v1_h1);
+  auto v1_h3 = new QHBoxLayout();
+  eventListenerGenerator(v1_h3);
   auto v1_h5 = new QHBoxLayout();
   protobufgenerator(v1_h5);
-  v1->addItem(v1_h1);
-  v1->addItem(v1_h2);
-  v1->addItem(v1_h3);
   v1->addItem(v1_h4);
+  v1->addItem(v1_h2);
+  v1->addItem(v1_h1);
+  v1->addItem(v1_h3);
   auto verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
   v1->addItem(verticalSpacer);
   this->setLayout(v1);
 }
-void ReactGenerator::exportgenerator(QHBoxLayout *hbox)
+void ReactGenerator::hookGenerator(QHBoxLayout *hbox)
 {
   auto g = new QGroupBox(this);
-  g->setTitle("create component prop");
+  g->setTitle("Hook Generator:");
   auto h = new QHBoxLayout(g);
   auto typeLabel = new QLabel(this);
-  auto typeselect = new QComboBox(this);
-  typeselect->addItem("let");
-  typeselect->addItem("const");
-  typeselect->addItem("class");
-  typeselect->addItem("function");
+  auto hookSelect = new QComboBox(this);
+  hookSelect->addItem("useState");
+  hookSelect->addItem("useRef");
+  hookSelect->addItem("useCallback");
+  hookSelect->addItem("useEffect");
   auto nameLabel = new QLabel(this);
   nameLabel->setText("variable name:");
-  auto name = new QLineEdit(this);
+  auto stateName = new QLineEdit(this);
+  stateName->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
   auto exportasLabel = new QLabel(this);
   exportasLabel->setText("export:");
   auto exporttval = new QLineEdit(this);
@@ -55,9 +57,9 @@ void ReactGenerator::exportgenerator(QHBoxLayout *hbox)
   auto checkbox = new QCheckBox(this);
   checkbox->setText("optional");
   h->addWidget(typeLabel);
-  h->addWidget(typeselect);
+  h->addWidget(hookSelect);
   h->addWidget(nameLabel);
-  h->addWidget(name);
+  h->addWidget(stateName);
   h->addWidget(exportasLabel);
   h->addWidget(exporttval);
   h->addWidget(defaultvalLabel);
@@ -65,38 +67,38 @@ void ReactGenerator::exportgenerator(QHBoxLayout *hbox)
   h->addWidget(checkbox);
   h->addWidget(resultLabel);
   h->addWidget(resultText);
-  auto generate = [typeselect, name, exporttval, defaultval, checkbox, resultText]() {
-    auto nameText = name->text().trimmed();
-    if (!nameText.isEmpty())
+  auto generate = [hookSelect, stateName, exporttval, defaultval, checkbox, resultText]() {
+    auto nameText = stateName->text().trimmed();
+    if (!nameText.isEmpty()) { nameText.replace(' ', '_'); }
+    QString r;
+    if (hookSelect->currentText() == "useState")
     {
-      if (nameText.front() == '$') { nameText.remove(0, 1); }
-      nameText.replace(' ', '_');
+      r += "const [" + nameText + ", set" + QString::fromStdString(pystring::capitalize(nameText.toStdString())) + "] = " + hookSelect->currentText() + "(";
+      if (!defaultval->toPlainText().isEmpty()) { r += defaultval->toPlainText(); }
+      r += ");";
     }
-    if (exporttval->text().isEmpty())
+    else if (hookSelect->currentText() == "useRef")
     {
-      QString r;
-      r += "export " + typeselect->currentText() + " " + nameText;
-      if (!defaultval->toPlainText().isEmpty()) { r += " = " + defaultval->toPlainText() + ";"; }
-      else
-      {
-        if (checkbox->isChecked()) { r += " = undefined;"; }
-        else
-        {
-          r += ";";
-        }
-      }
-      resultText->setText(r);
+      r += "const " + nameText + " = " + hookSelect->currentText() + "(";
+      if (!defaultval->toPlainText().isEmpty()) { r += defaultval->toPlainText(); }
+      r += ");";
     }
-    else
+    else if (hookSelect->currentText() == "useCallback")
     {
-      QString r;
-      r += typeselect->currentText() + " " + nameText + ";\n";
-      r += "export { " + nameText + " as " + exporttval->text() + " };";
-      resultText->setText(r);
+      r += "const " + nameText + " = " + hookSelect->currentText() + "(";
+      if (!defaultval->toPlainText().isEmpty()) { r += defaultval->toPlainText(); }
+      r += ");";
     }
+    else if (hookSelect->currentText() == "useEffect")
+    {
+      r += hookSelect->currentText() + "(";
+      if (!defaultval->toPlainText().isEmpty()) { r += defaultval->toPlainText(); }
+      r += ");";
+    }
+    resultText->setText(r);
   };
-  connect(typeselect, &QComboBox::currentTextChanged, generate);
-  connect(name, &QLineEdit::textEdited, generate);
+  connect(hookSelect, &QComboBox::currentTextChanged, generate);
+  connect(stateName, &QLineEdit::textEdited, generate);
   connect(exporttval, &QLineEdit::textEdited, generate);
   connect(defaultval, &QPlainTextEdit::textChanged, generate);
   connect(checkbox, &QCheckBox::clicked, generate);
@@ -108,20 +110,20 @@ void ReactGenerator::exportgenerator(QHBoxLayout *hbox)
   });
   hbox->addWidget(g);
 }
-void ReactGenerator::reactivestatgenerator(QHBoxLayout *hbox)
+void ReactGenerator::componentSkeletonGenerator(QHBoxLayout *hbox)
 {
   auto g = new QGroupBox(this);
-  g->setTitle("$: marks a statement as reactive");
+  g->setTitle("component Skeleton Generator:");
   auto h = new QHBoxLayout(g);
   auto nameLabel = new QLabel(this);
-  nameLabel->setText("variable name:");
-  auto name = new QLineEdit(this);
+  nameLabel->setText("component name:");
+  auto componentName = new QLineEdit(this);
+  componentName->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
   auto simplevalueLabel = new QLabel(this);
   simplevalueLabel->setText("Simple Value:");
-  auto simplevalue = new QLineEdit(this);
-  simplevalue->setToolTip(
-      "If a statement consists entirely of an assignment to an undeclared "
-      "variable, Svelte will inject a let declaration on your behalf.");
+  auto propInterfaceValue = new QLineEdit(this);
+  propInterfaceValue->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+  propInterfaceValue->setToolTip("PropInterface");
   auto functionvalueLabel = new QLabel(this);
   functionvalueLabel->setText("OR  Function Value:");
   auto functionvalue = new QPlainTextEdit(this);
@@ -129,81 +131,67 @@ void ReactGenerator::reactivestatgenerator(QHBoxLayout *hbox)
   resultLabel->setText("result:");
   auto resultText = new QTextEdit(this);
   h->addWidget(nameLabel);
-  h->addWidget(name);
+  h->addWidget(componentName);
   h->addWidget(simplevalueLabel);
-  h->addWidget(simplevalue);
+  h->addWidget(propInterfaceValue);
   h->addWidget(functionvalueLabel);
   h->addWidget(functionvalue);
   h->addWidget(resultLabel);
   h->addWidget(resultText);
-  auto generate = [name, simplevalue, functionvalue, resultText]() {
-    auto nameText = name->text().trimmed();
-    if (!nameText.isEmpty())
-    {
-      if (nameText.front() == '$') { nameText.remove(0, 1); }
-      nameText.replace(' ', '_');
-    }
+  connect(componentName, &QLineEdit::textEdited, propInterfaceValue, &QLineEdit::setText);
+  auto generate = [componentName, propInterfaceValue, functionvalue, resultText]() {
+    auto nameText = componentName->text().trimmed();
+    if (!nameText.isEmpty()) { nameText.replace(' ', '_'); }
     QString r;
-    r += "$: ";
-    if (!simplevalue->text().isEmpty()) { r += nameText + " = " + simplevalue->text() + ";"; }
+    if (!propInterfaceValue->text().isEmpty())
+    {
+      r += R"(interface )";
+      r += propInterfaceValue->text();
+      r += R"(Props {
+
+}\n)";
+    }
+    r += "const ";
+    r += nameText + " = (props";
+    if (!propInterfaceValue->text().isEmpty()) { r += ": " + propInterfaceValue->text() + "Props)"; }
     else
     {
-      r += "{\n" + functionvalue->toPlainText() + "\n}";
+      r += ")";
     }
+    r += " => {\n";
+    r += functionvalue->toPlainText();
+    r += "\n}";
     resultText->setText(r);
   };
-  connect(name, &QLineEdit::textEdited, generate);
-  connect(simplevalue, &QLineEdit::textEdited, generate);
+  connect(componentName, &QLineEdit::textEdited, generate);
+  connect(propInterfaceValue, &QLineEdit::textEdited, generate);
   connect(functionvalue, &QPlainTextEdit::textChanged, generate);
   hbox->addWidget(g);
 }
-void ReactGenerator::elementdirectivesgenerator(QHBoxLayout *hbox)
+void ReactGenerator::eventListenerGenerator(QHBoxLayout *hbox)
 {
   auto g = new QGroupBox(this);
-  g->setTitle("Element directives");
+  g->setTitle("Event Listners:");
   auto h = new QHBoxLayout(g);
   auto directiveLabel = new QLabel(this);
   auto directiveselect = new QComboBox(this);
-  directiveselect->addItem("on:");
-  directiveselect->addItem("bind:");
-  // directiveselect->addItem("bind:innerHTML");
-  // directiveselect->addItem("bind:contenteditable");
-  // bind:duration
-  // bind:buffered
-  // bind:seekable
-  // bind:played
-  // bind:currentTime
-  // bind:paused
-  // bind:volume
-  // clientWidth
-  // clientHeight
-  // offsetWidth
-  // offsetHeight
-  // introstart
-  // introend
-  // outrostart
-  // outroend
-  directiveselect->addItem("bind:group");
-  directiveselect->addItem("bind:this");
-  directiveselect->addItem("class:");
-  directiveselect->addItem("use:");
-  directiveselect->addItem("transition:");
-  directiveselect->addItem("in:");
-  directiveselect->addItem("out:");
-  directiveselect->addItem("animate:");
+  directiveselect->addItem("on");
   auto eventnameLabel = new QLabel(this);
   eventnameLabel->setText("name:");
   auto eventname = new QLineEdit(this);
+  eventname->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
   auto modifiersLabel = new QLabel(this);
   modifiersLabel->setText("Modifiers");
   auto modifiersselect = new QListWidget(this);
-  modifiersselect->setSelectionMode(QAbstractItemView::MultiSelection);
+  // http://qt.shoutwiki.com/wiki/How_to_filter_QListView
+  modifiersselect->setSelectionMode(QAbstractItemView::SingleSelection);
+  modifiersselect->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
   modifiersselect->addItem("");
-  modifiersselect->addItem("preventDefault");
-  modifiersselect->addItem("stopPropagation");
-  modifiersselect->addItem("passive");
-  modifiersselect->addItem("capture");
-  modifiersselect->addItem("once");
+  modifiersselect->addItem("Change");
+  modifiersselect->addItem("Click");
+  modifiersselect->addItem("Focus");
+  modifiersselect->addItem("Blue");
+  modifiersselect->addItem("Input");
   auto handlerLabel = new QLabel(this);
   handlerLabel->setText("Handler:");
   auto handlertval = new QPlainTextEdit(this);
@@ -220,28 +208,13 @@ void ReactGenerator::elementdirectivesgenerator(QHBoxLayout *hbox)
   h->addWidget(handlertval);
   h->addWidget(resultLabel);
   h->addWidget(resultText);
-  auto generate = [directiveselect, eventname, modifiersselect, handlertval, resultText]() {
-    // on:eventname={handler}
-    // on:eventname|modifiers={handler}
-    auto nameText = eventname->text().trimmed();
-    if (!nameText.isEmpty())
-    {
-      if (nameText.front() == '$') { nameText.remove(0, 1); }
-      nameText.replace(' ', '_');
-    }
-    auto handleText = handlertval->toPlainText();
-    bool isSame = nameText == handleText;
+  connect(modifiersselect, &QListWidget::currentTextChanged, eventname, &QLineEdit::setText);
+  auto generate = [directiveselect, eventname, handlertval, resultText]() {
     QString r = directiveselect->currentText();
+    auto nameText = QString::fromStdString(pystring::capitalize(eventname->text().trimmed().toStdString()));
+    if (!nameText.isEmpty()) { nameText.replace(' ', '_'); }
     r += nameText;
-    std::vector<std::string> v;
-    for (auto &item : modifiersselect->selectedItems()) { v.push_back(item->text().toStdString()); }
-    auto j = pystring::join("|", v);
-    if (!j.empty()) { r += "|"; }
-    r += QString::fromStdString(j);
-    if (!handlertval->toPlainText().isEmpty())
-    {
-      if (!isSame || directiveselect->currentText() == "on:") { r += "='{" + handlertval->toPlainText() + "}'"; }
-    }
+    if (!handlertval->toPlainText().isEmpty()) { r += "={" + handlertval->toPlainText() + "}"; }
     resultText->setText(r);
   };
   connect(directiveselect, QOverload<int>::of(&QComboBox::currentIndexChanged), [directiveselect, modifiersLabel, eventnameLabel, eventname, modifiersselect, handlerLabel]() {
@@ -340,7 +313,9 @@ void ReactGenerator::importgenerator(QHBoxLayout *hbox)
   auto importLabel = new QLabel(this);
   importLabel->setText("import:");
   auto importselect = new QListWidget(this);
+  importselect->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
   importselect->setSelectionMode(QAbstractItemView::MultiSelection);
+  importselect->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
   auto resultLabel = new QLabel(this);
   resultLabel->setText("result:");
   auto resultText = new QTextEdit(this);
--- widgets/react/reactgenerator.h
+++ widgets/react/reactgenerator.h
@@ -1,24 +1,21 @@
 #pragma once
-
 #include <QWidget>
 #include <QtWidgets>
-namespace widgets {
-
-class ReactGenerator : public QWidget {
+namespace widgets
+{
+class ReactGenerator : public QWidget
+{
   Q_OBJECT
- private:
+private:
   void init();
-  void exportgenerator(QHBoxLayout *hbox);
-  void reactivestatgenerator(QHBoxLayout *hbox);
-  void elementdirectivesgenerator(QHBoxLayout *hbox);
+  void hookGenerator(QHBoxLayout *hbox);
+  void componentSkeletonGenerator(QHBoxLayout *hbox);
+  void eventListenerGenerator(QHBoxLayout *hbox);
   void importgenerator(QHBoxLayout *hbox);
   void protobufgenerator(QHBoxLayout *hbox);
-
- public:
+public:
   explicit ReactGenerator(QWidget *parent = nullptr);
   ~ReactGenerator();
-
- private:
+private:
 };
 }  // namespace widgets
-
